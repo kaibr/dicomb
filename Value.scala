5f178c374f847c3d2741be732360f86060206906
kaibr <kai.bruennler@gmail.com>
2011-01-15 19:23:56 +0100

    value pretty printing

--- Value.scala
+++ Value.scala
@@ -2,7 +2,15 @@ package dicomb;
 
 sealed abstract class Value {}
 
-case class Integer(i:Int) extends Value {}
-case class Pair(left: Value, right: Value) extends Value {}
-case class Closure(v:Value,p1:Prog,p2:Prog) extends Value {}
+case class Integer(i:Int) extends Value {
+	override def toString = i.toString
+}
+case class Pair(left: Value, right: Value) extends Value {
+	override def toString = "("+left+","+right+")"
+}
+
+// Closure corresponds to a term v.i.(p1 -> p2)
+case class Closure(v:Value,p1:Prog,p2:Prog) extends Value {
+	override def toString = "{"+v+","+p1+","+p2+"}"
+}
 
