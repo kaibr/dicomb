7a94be52dc0908ec696f41b5cf5e2db00880c949
Kai Bruennler <kai.bruennler@gmail.com>
2011-02-16 21:43:23 +0100

    formatting cleanup

--- interpreter.scala
+++ interpreter.scala
@@ -10,14 +10,29 @@ case class ExecException(s: String) extends Exception(s) {}
 
 def execute(s:State):Value = s match
 {
-	case (v,  Seq(p1,p2)) 									=> execute(execute(v,p1),p2)
-	case (Pair(v1,v2),  Conj(p1,p2)) 					=> Pair(execute(v1,p1),execute(v2,p2))
-	case (Closure(v,p3,p4),  Imp(p1,p2)) 			=> Closure(v,Seq(p1,p3),Seq(p4,p2))
-	case (v,  Rule("c")) 										=> Pair(v,v)
-	case (Pair(v1,v2),  Rule("w1")) 					=> v1
-	case (Pair(v1,v2),  Rule("w2")) 					=> v2
-	case (v,  Rule("i")) 										=> Closure(v,Id(),Id())
-	case (Pair(Closure(v1,p1,p2),v2),  Rule("e"))	=> execute(Pair(v1,execute(v2,p1)),p2)
+	case (v,  Seq(p1,p2))                                   
+	=> execute(execute(v,p1),p2)
+	
+	case (Pair(v1,v2),  Conj(p1,p2))
+	=> Pair(execute(v1,p1),execute(v2,p2))
+	
+	case (Closure(v,p3,p4),  Imp(p1,p2))
+	=> Closure(v,Seq(p1,p3),Seq(p4,p2))
+	
+	case (v,  Rule("c"))
+	=> Pair(v,v)
+	
+	case (Pair(v1,v2),  Rule("w1"))
+	=> v1
+	
+	case (Pair(v1,v2),  Rule("w2"))
+	=> v2
+	
+	case (v,  Rule("i"))
+	=> Closure(v,Id(),Id())
+	
+	case (Pair(Closure(v1,p1,p2),v2),  Rule("e"))	
+	=> execute(Pair(v1,execute(v2,p1)),p2)
 
 	case _ => throw new ExecException("cannot execute " + s)
 	
