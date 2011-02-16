8126d2a9327f6a9de3cd939e9a72e36ccf7fb73f
Kai Bruennler <kai.bruennler@gmail.com>
2011-02-16 21:15:12 +0100

    do not override toString in Prog, we need it. Add prettyprint instead

--- Prog.scala
+++ Prog.scala
@@ -1,26 +1,22 @@
 
 package dicomb;
 
-sealed abstract class Prog {}
-
-case class Rule(name: String) extends Prog {
-  override def toString = name
-}
-case class Id() extends Prog {
-  override def toString = "id"
-}
-
-case class Seq(p: Prog, q: Prog) extends Prog {
-  override def toString = p+"."+q
+sealed abstract class Prog {
+	
+	def prettyprint : String =  this match {
+		case  Rule(name: String) => name
+		case Id() => "id"
+		case Seq(p: Prog, q: Prog) => p.prettyprint +"."+ q.prettyprint
+		case Conj(p: Prog, q: Prog) =>  "("+p.prettyprint+"^"+q.prettyprint+")"
+		case Imp(p: Prog, q: Prog) =>  "("+p.prettyprint+"->"+q.prettyprint+")"
+	} 
 }
 
-case class Conj(p: Prog, q: Prog) extends Prog {
-  override def toString = "("+p+"^"+q+")"
-}
-
-case class Imp(p: Prog, q: Prog) extends Prog {
-  override def toString = "("+p+"->"+q+")"
-}
+case class Rule(name: String) extends Prog {}
+case class Id() extends Prog {}
+case class Seq(p: Prog, q: Prog) extends Prog {}
+case class Conj(p: Prog, q: Prog) extends Prog {}
+case class Imp(p: Prog, q: Prog) extends Prog {}
 
 
 
